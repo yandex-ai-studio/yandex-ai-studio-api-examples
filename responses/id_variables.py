2dbd2a08af91dcc5f83a5022732135c15f4b1b90
anakarimova <anastasiia.karimova@yandex.ru>
2026-02-19 00:27:22 +0300

    Update endpoint [id_variables.py]

--- responses/id_variables.py
+++ responses/id_variables.py
@@ -6,7 +6,7 @@ YANDEX_CLOUD_MODEL = "yandexgpt"
 
 client = openai.OpenAI(
     api_key=YANDEX_CLOUD_API_KEY,
-    base_url="https://rest-assistant.api.cloud.yandex.net/v1",
+    base_url="https://ai.api.cloud.yandex.net/v1",
     project=YANDEX_CLOUD_FOLDER
 )
 response = client.responses.create(
