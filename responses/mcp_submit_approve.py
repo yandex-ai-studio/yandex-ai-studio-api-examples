4c28e916ebfaac6fb58e34ac3fe6d82edb76af70
anakarimova <anastasiia.karimova@yandex.ru>
2026-02-19 00:29:45 +0300

    Update endpoint [mcp_submit_approve.py]

--- responses/mcp_submit_approve.py
+++ responses/mcp_submit_approve.py
@@ -51,7 +51,7 @@ def main():
     client = openai.OpenAI(
         api_key=YANDEX_CLOUD_API_KEY,
         project=YANDEX_CLOUD_FOLDER,
-        base_url="https://rest-assistant.api.cloud.yandex.net/v1",
+        base_url="https://ai.api.cloud.yandex.net/v1",
     )
 
     # Первый запрос
